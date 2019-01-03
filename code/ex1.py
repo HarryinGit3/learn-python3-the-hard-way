fe7158c023e0de25545d46a1d5a6b06d208332fa
Harryin <harryin123@qq.com>
2019-01-03 16:42:37 +0800

    添加ex1

---
+++ code/ex1.py
@@ -0,0 +1,51 @@
+# 原代码
+print("Hello World")
+print("Hello Again")
+print("I like typing this.")
+print("This is fun.")
+print("Yay! Printing.")
+print("I'd much rather you 'not'.")
+print('I "said" do not touch this')
+
+print("-------------------------")
+# 练习
+'''
+	1. 让你的脚本再多打印一行。
+	2. 让你的脚本只打印其中一行。
+    3. 在一行的起始位置放一个“#”字符。它的作用是什么，自己研究一下。
+'''
+# 1. 多打印一行
+print("Hello World")
+print("Hello Again")
+print("I like typing this.")
+print("This is fun.")
+print("Yay! Printing.")
+print("I'd much rather you 'not'.")
+print('I "said" do not touch this')
+print("多打印一行")
+
+print("-------------------------")
+# 2. 让你的脚本只打印其中一行。
+print("Hello World")
+# print("Hello Again")
+# print("I like typing this.")
+# print("This is fun.")
+# print("Yay! Printing.")
+# print("I'd much rather you 'not'.")
+# print('I "said" do not touch this')
+# print("多打印一行")
+
+# 3. 在一行的起始位置放一个“#”字符。它的作用是什么，自己研究一下。
+print("Hello World")
+print("Hello Again")
+# print("I like typing this.")
+print("This is fun.")
+print("Yay! Printing.")
+print("I'd much rather you 'not'.")
+print('I "said" do not touch this')
+print("多打印一行")
+
+
+
+
+
